1d518d80e082bd2f23da668f0dd4abee651e27d3
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2023-09-16 21:55:33 -0400

    Update drugs.js

--- drugs.js
+++ drugs.js
@@ -4,22 +4,25 @@ class Database {
         this.property_dicts = new Map();
         this.index_properties = new Array();
     }
+    search_in_drug_info(drug_infos, property) {}
     update_database(json) {
         // clear previous data
-        for (property in update_database.index_properties) {
-            update_database.property_dicts.delete(property);
+        for (property in this.index_properties) {
+            this.property_dicts.delete(property);
         }
         this.index_properties = new Array();
         // parse new json
         this.original_database = json;
         // set new pointers to original_database elements in property_dicts
-        for (const drugInfo in this.original_database) {
-            for (const [property, propertyData] in drugInfo) {
+        for (const drugInfo of this.original_database) {
+            for (const [property, propertyData] of Object.entries(drugInfo)) {
                 if (!this.index_properties.includes(property)) {
                     // add new unseen property to all in database, should not occur in the current database except on the first drugInfo
+                    /*
                     for (const drugInfo in this.original_database) {
                         drugInfo[property] = null;
                     }
+                    */
                     this.index_properties.push(property);
                     this.property_dicts.set(property, new Map());
                 }
@@ -32,13 +35,34 @@ class Database {
             }
         }
     }
+    get_drugs_by_property(property) {
+        let property_dict = this.property_dicts.get(property)
+        return property_dict;
+    }
     get_drug_info_by_property(property, propertyData) {
         // example: get_drug_info_by_property("ActiveIngredient", "ACETAMINOPHEN")
         // example: get_drug_info_by_property("DrugName", "TYLENOL")
-        let property_dict = this.property_dicts.get(property)
-        return property_dict.get(propertyData);
+        let property_dict = this.get_drugs_by_property(property);
+        let property_dict_entry = property_dict.get(propertyData);
+        return property_dict_entry;
+    }
+    verify_data(drugNameOrActiveIngredient, form=null, strength=null) {
+        try {
+            possibleDrugInfos = new Array();
+            possibleDrugInfos.push(get_drug_info_by_property("ActiveIngredient", drugNameOrActiveIngredient));
+            possibleDrugInfos.push(get_drug_info_by_property("DrugName", drugNameOrActiveIngredient));
+        } catch (e) {
+            if (e instanceof ReferenceError) {
+                return ReferenceError("drug name or active ingredient not found")
+            } else {
+                throw e;
+            }
+        };
+
     }
 }
+
+var database = null
 
 /*
 var req = new XMLHttpRequest();
@@ -48,7 +72,8 @@ req.onload = function(){
 req.open('GET', './output-onlinetsvtools.json');
 req.send();
 */
-
-fetch('./drugs.json')
-    .then((response) => response.json())
-    .then((json) => update_database(json));
+window.addEventListener('load', function() {
+    fetch('./drugs.json')
+        .then((response) => response.json())
+        .then((json) => {database = new Database(); database.update_database(json);});
+})
